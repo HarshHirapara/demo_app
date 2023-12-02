7fa5be28e1ef63aa32f7b47d79333a79b62522d8
HarshHirapara <harsh.h@joflee.com>
2023-12-02 17:19:29 +0530

    implement-search

--- lib/core/database/getx_functions.dart
+++ lib/core/database/getx_functions.dart
@@ -1,7 +1,8 @@
-import 'package:demo_app/core/model/user_model_class.dart';
 import 'package:get/get_rx/get_rx.dart';
 
 class GetXFunctions {
   static RxList<Map<String, dynamic>> userList = <Map<String, dynamic>>[].obs;
-  static RxList<UserModel> favoriteList = <UserModel>[].obs;
+  static RxList<Map<String, dynamic>> foundUser = <Map<String, dynamic>>[].obs;
+  static RxList<Map<String, dynamic>> favoriteList =
+      <Map<String, dynamic>>[].obs;
 }
