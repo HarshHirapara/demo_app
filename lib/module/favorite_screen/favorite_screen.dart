5bc996573c64006b75a4e07848b31e2b0d01ca8d
HarshHirapara <harsh.h@joflee.com>
2023-12-04 09:54:16 +0530

    [implement]-complete user favorite functionality

--- lib/module/favorite_screen/favorite_screen.dart
+++ lib/module/favorite_screen/favorite_screen.dart
@@ -1,6 +1,7 @@
 import 'package:demo_app/core/database/getx_functions.dart';
 import 'package:demo_app/module/widget/common_widget_user_card.dart';
 import 'package:flutter/material.dart';
+import 'package:get/state_manager.dart';
 
 class FavoriteUserScreen extends StatelessWidget {
   const FavoriteUserScreen({super.key});
@@ -11,10 +12,12 @@ class FavoriteUserScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Favorite Users'),
       ),
-      body: ListView.builder(
-        itemCount: GetXFunctions.favoriteList.length,
-        itemBuilder: (context, index) =>
-            UserCard(index: index, user: GetXFunctions.favoriteList),
+      body: Obx(
+        () => ListView.builder(
+          itemCount: GetXFunctions.favoriteList.length,
+          itemBuilder: (context, index) =>
+              UserCard(index: index, user: GetXFunctions.favoriteList),
+        ),
       ),
     );
   }
